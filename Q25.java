3f46079f15bcb8c8d0fc73660fbe1ce7f29fccf9
Sambit Samaya Sarangi <43579824+sambitsamayasarangi7080@users.noreply.github.com>
2021-02-06 23:12:33 +0530

    Add files via upload

---
+++ Q25.java
@@ -0,0 +1,11 @@
+/*
+ * Q25-Find longest coinsecutive subsequence.
+ */
+import java.util.*;
+public class Q25 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+    
+}

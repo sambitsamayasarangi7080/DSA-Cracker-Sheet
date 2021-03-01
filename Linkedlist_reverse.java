d0319a63e67788a09f05e187b693b0085eb949bc
Sambit Samaya Sarangi <43579824+sambitsamayasarangi7080@users.noreply.github.com>
2021-03-01 23:13:24 +0530

    Add files via upload

---
+++ Linkedlist_reverse.java
@@ -0,0 +1,16 @@
+public class Linkedlist_reverse {
+    static Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}

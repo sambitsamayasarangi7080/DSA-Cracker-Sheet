12650d586e5a4cd85ec65a7e08dfb22f7975c6df
Sambit Samaya Sarangi <43579824+sambitsamayasarangi7080@users.noreply.github.com>
2021-03-03 23:12:39 +0530

    Add files via upload

--- Linkedlist_reverse.java
+++ Linkedlist_reverse.java
@@ -8,9 +8,42 @@ public class Linkedlist_reverse {
             next=null;
         }
     }
+    void printlist(Node head){
+        if(head==null)
+            return ;
+        while(head!=null){
+            System.out.print(head.data+"->");
+            head=head.next;
+        }    
+    }
+
+    static Node reverse_iterative(Node head){
+        Node dum=null;
+        Node curr=head,next=null;
+        while(head!=null){
+            curr=head.next;
+            head.next=dum;
+            dum=head;
+            head=curr;
+
+        }
+        head=dum;
+        return head;
+    }
 
     public static void main(String[] args) {
-        
+        Linkedlist_reverse llist=new Linkedlist_reverse();
+        llist.head=new Node(1);
+        llist.head.next=new Node(2);
+        llist.head.next.next=new Node(3);
+        llist.head.next.next.next=new Node(4);
+        //llist.head.next.next.next.next=new Node(5);
+
+        llist.printlist(head);
+        head=llist.reverse_iterative(head);
+        System.out.println("\n");
+        System.out.println("After Reverse:");
+        llist.printlist(head);
     }
     
 }
